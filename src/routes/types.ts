061ca7153930bc8e27b7fd7445868014c0098899
sidney-prytherch <sidneyprytherch@gmail.com>
2024-08-22 20:17:24 -0400

    Create projects.json and projectTags enum
    project section of site gets its data from json

---
+++ src/routes/types.ts
@@ -0,0 +1,11 @@
+export enum ProjectTags {
+    WEB = "Web",
+    ANDROID = "Android",
+    DESKTOPAPP = "Desktop App",
+    SVELTE = "Svelte",
+    HTML = "HTML",
+    CSS = "CSS",
+    JAVASCRIPT = "JavaScript",
+    TYPESCRIPT = "TypeScript",
+    JAVA = "Java"
+}
